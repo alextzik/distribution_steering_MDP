a4b9621b90adeafb81e4f240ab40dba0e2d5ad50
Alexandros Tzikas <33734693+alextzik@users.noreply.github.com>
2025-09-16 19:41:46 -0700

    EDIT: Minor

--- main.py
+++ main.py
@@ -465,14 +465,14 @@ def dyn_func(x, u):
 
 dyns = dynamics(3, 2, dyn_func)
 
-num_steps = 350
+num_steps = 100
 
 # intiial state
 state = State()
 init_mean = np.array([-2, -2., 0.])
 init_cov = np.eye(3)
 init_cov[2,2]=0.
-state.sample(mean = init_mean, covariance=init_cov, num_samples=1000)
+state.sample(mean = init_mean, covariance=init_cov, num_samples=3000)
 baseline_state_samples = state.samples
 
 # Target density
@@ -488,14 +488,14 @@ qs = dirs
 
 quantiles = []
 for q in qs:
-    proj_samples = q@target_state.means[0].reshape(-1,1) + np.sqrt(q@target_state.covs[0]@q)*np.random.standard_normal(size=(1000,))
+    proj_samples = q@target_state.means[0].reshape(-1,1) + np.sqrt(q.T@target_state.covs[0]@q)*np.random.standard_normal(size=(1000,))
     quantiles.append(np.quantile(proj_samples, 0.9))
     quantiles.append(np.quantile(proj_samples, 0.1))
 
 
-bs = np.linspace(np.min(np.array(quantiles)), np.max(np.array(quantiles)), 100)
+bs = np.linspace(np.min(np.array(quantiles)), np.max(np.array(quantiles)), 200)
 bs = np.tile(bs, pars.NUM_HALFSPACES).reshape(-1,1)
-qs = np.repeat(qs.T, 100, axis=1)
+qs = np.repeat(qs.T, 200, axis=1)
 
 
 target_state.compute_prob_contents(qs, bs)
@@ -524,7 +524,10 @@ for t in tqdm(range(num_steps)):
 
     dists_gradient.append(compute_heur_dist(baseline_state_samples, mcts.target_state, qs, bs)) 
 
-    baseline_state_samples, _, __ = baseline_algorithm(baseline_state_samples.copy(), 3, 2, target_state, qs, bs, None, None)
+    baseline_state_samples = baseline_algorithm(baseline_state_samples.copy(), 
+                                                 3, 2, 
+                                                 target_state, qs, bs, 
+                                                 n_dyn_steps=25)
 
 
 np.save("dists_gradient_baseline.npy", dists_gradient)
